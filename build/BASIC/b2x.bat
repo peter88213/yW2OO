a2ac0b40a60d041c956163004e8d91fbe69a8959
Peter <Peter@BOX.fritz.box>
2018-10-04 12:32:42 +0200

    Added setup folder generator cs.bat

--- build/BASIC/b2x.bat
+++ build/BASIC/b2x.bat
@@ -7,11 +7,16 @@ REM see: https://github.com/peter88213/yW2OO
 REM license: The MIT License (https://opensource.org/licenses/mit-license.php)
 REM copyright: (c) 2018, Peter Triesberger
 REM version: v1.0.0
-@echo on
+
+set _src_path=..\..\src\
+set _macro_path=..\OXT\
 set _header=header.txt
 set _footer=footer.txt
-set _source=Convert.BAS
-set _xml=Convert.xba
+set _source=%_src_path%Convert.BAS
+set _xml=%_macro_path%Convert.xba
+
+@echo on
+
 copy /y %_source% temp.txt
 sar.py temp.txt b2x.txt
 type %_header%  > %_xml%
