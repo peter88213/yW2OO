a23b051528614a6d3772bdd4a8b07099e5dd8cfe
peter88213 <peter.88213@t-online.de>
2020-02-23 10:52:55 +0100

    Update tests

--- test/yw2ootest.py
+++ test/yw2ootest.py
@@ -24,12 +24,15 @@ TEST_EXEC_PATH = 'yw7/'
 # To be placed in TEST_DATA_PATH:
 
 # Test data
-TEST_FILE = 'yWriter Sample Project.yw7'
-EXPORT_FILE = 'yWriter Sample Project.odt'
+YW7_TEST = TEST_EXEC_PATH + 'yWriter Sample Project.yw7'
+ODT_TEST = 'yWriter Sample Project.odt'
 
 DOCUMENT_CONTENT = 'content.xml'
 DOCUMENT_META = 'meta.xml'
 DOCUMENT_STYLES = 'styles.xml'
+
+YW7_NORMAL = TEST_DATA_PATH + 'normal.yw7'
+DOC_NORMAL = TEST_DATA_PATH + 'normal.odt'
 
 
 def read_file(inputFile):
@@ -50,32 +53,44 @@ def copy_file(inputFile, outputFile):
     return()
 
 
+def remove_all_testfiles():
+    try:
+        os.remove(YW7_TEST)
+    except:
+        pass
+    try:
+        os.remove(TEST_EXEC_PATH + DOCUMENT_STYLES)
+    except:
+        pass
+    try:
+        os.remove(TEST_EXEC_PATH + DOCUMENT_CONTENT)
+    except:
+        pass
+    try:
+        os.remove(TEST_EXEC_PATH + ODT_TEST)
+    except:
+        pass
+
+
 class NormalOperation(unittest.TestCase):
     """Test case: Normal operation."""
 
     def setUp(self):
-        os.chdir(TEST_PATH)
+
         try:
-            os.remove(TEST_EXEC_PATH + TEST_FILE)
+            os.remove(TEST_EXEC_PATH + ODT_TEST)
         except:
             pass
         # Place the correct html Export file.
-        copy_file(TEST_DATA_PATH + TEST_FILE,
-                  TEST_EXEC_PATH + TEST_FILE)
-        try:
-            os.remove(TEST_EXEC_PATH + EXPORT_FILE)
-        except:
-            pass
+        copy_file(YW7_NORMAL,
+                  YW7_TEST)
 
     def test_yw2oo(self):
-        """ Test yw2oo. """
         os.chdir(TEST_EXEC_PATH)
-
-        yw2oo.main()
-        # self.assertEqual(, 'SUCCESS: "' + EXPORT_FILE + '" saved.')
+        self.assertEqual(yw2oo.main(), 'SUCCESS: "' + ODT_TEST + '" saved.')
         os.chdir(TEST_PATH)
 
-        with zipfile.ZipFile(TEST_EXEC_PATH + EXPORT_FILE, 'r') as myzip:
+        with zipfile.ZipFile(TEST_EXEC_PATH + ODT_TEST, 'r') as myzip:
             myzip.extract(DOCUMENT_CONTENT, TEST_EXEC_PATH)
             myzip.extract(DOCUMENT_STYLES, TEST_EXEC_PATH)
 
@@ -84,23 +99,26 @@ class NormalOperation(unittest.TestCase):
         self.assertEqual(read_file(TEST_EXEC_PATH + DOCUMENT_STYLES),
                          read_file(TEST_DATA_PATH + DOCUMENT_STYLES))
 
+    def tearDown(self):
+        remove_all_testfiles()
+
 
 class NoProjectFile(unittest.TestCase):
     """Test case: yWriter project file is not present."""
 
     def setUp(self):
-        os.chdir(TEST_PATH)
         # Make sure there's no yWriter project file present.
         try:
-            os.remove(TEST_EXEC_PATH + TEST_FILE)
+            os.remove(YW7_TEST)
         except:
             pass
 
     def test_all(self):
         """ Test both yw2oo and sceti. """
-        os.chdir(TEST_EXEC_PATH)
         self.assertEqual(yw2oo.main(), 'ERROR: No yWriter 7 project found.')
-        os.chdir(TEST_PATH)
+
+    def tearDown(self):
+        remove_all_testfiles()
 
 
 def main():
